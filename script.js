bb9658f1b2a7840a916f8bfec7d688f0bf8e0fb7
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-28 13:27:14 -0400

    Change input style to screen and buffer to array based system

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
 const buttons = document.querySelectorAll(".btn");
-let inputA = document.querySelector("#inputA");
-let inputB = document.querySelector("#inputB");
+let screen = document.querySelector("#inputA");
+let buffer = document.querySelector("#inputB");
+
+let screenText = [];
+let bufferText = [];
 
 buttons.forEach((button) =>
   button.addEventListener("mousedown", () => {
+    if (button.id == "all-clear") {
+      zeroBuffer();
+      zeroScreen();
+    }
+    if (button.id == "clear") {
+      screen.textContent == "" ? zeroBuffer() : zeroScreen();
+    }
     if (button.className.includes("num")) {
-      inputA.textContent += button.id;
-      console.log(inputA);
+      (screenText.length < 14) ? 
+      (screenText.push(button.id)),
+      (updateScreen())
+    }
+    if (button.className.includes("operator")) {
+      bufferText = screenText;
+      updateBuffer();
+      zeroScreen();
+    }
+    if (button.id == "equal") {
     }
   })
 );
+
+const add = function (x, y) {
+  return x + y;
+};
+const subtract = function (x, y) {
+  return x - y;
+};
+const multiply = function (x, y) {
+  return x * y;
+};
+const divide = function (x, y) {
+  return x / y;
+};
+const equal = function () {};
+
+// UPDATE FUNCTIONS
+const updateBuffer = function () {
+  buffer.textContent = bufferText.join("");
+};
+const updateScreen = function () {
+  screen.textContent = screenText.join("");
+};
+
+// ZERO FUNCTIONS
+const zeroScreen = function () {
+  screenText = [];
+  screen.textContent = screenText.join();
+};
+const zeroBuffer = function () {
+  bufferText = [];
+  buffer.textContent = bufferText.join();
+};
+
+// make array using textContent in buffer with textContent on screen
+// array is 3 items [num1, op, num2]
+// check op in array, run function on two numbers depending on the op
 
 // get button id
 // if number add number
