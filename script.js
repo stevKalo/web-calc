c5bae394ddc0455d4213d36e9b5d035c286714d7
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-09-29 12:11:48 -0400

    Change scripting to string based system, complete functionality

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const buttons = document.querySelectorAll(".btn");
 let screen = document.querySelector("#inputA");
 let buffer = document.querySelector("#inputB");
 
-let screenText = [];
-let bufferText = [];
-
 buttons.forEach((button) =>
   button.addEventListener("mousedown", () => {
     if (button.id == "all-clear") {
@@ -15,19 +12,39 @@ buttons.forEach((button) =>
       screen.textContent == "" ? zeroBuffer() : zeroScreen();
     }
     if (button.className.includes("num")) {
-      if (screenText.length < 15) {
-        screenText.push(button.id);
-        updateScreen();
-      }
+      screen.textContent += button.id;
     }
     if (button.className.includes("operator")) {
-      bufferText.push(screenText.join(""));
-      bufferText.push(button.id);
-      updateBuffer();
-      zeroScreen();
+      if (buffer.textContent == "") {
+        buffer.textContent = screen.textContent + ` ${button.id}`;
+        zeroScreen();
+      } else if (buffer.textContent != "" && screen.textContent == "") {
+        let newBuffer = buffer.textContent.slice(0, -1);
+        buffer.textContent = newBuffer + button.id;
+      } else if (buffer.textContent != "" && screen.textContent != "") {
+        equal();
+        buffer.textContent = screen.textContent + ` ${button.id}`;
+        zeroScreen();
+      }
     }
     if (button.id == "equal") {
-      equal();
+      if (buffer.textContent && screen.textContent) {
+        equal();
+      } else if (
+        (screen.textContent == "8008" && buffer.textContent == "") ||
+        (screen.textContent == "318008" && buffer.textContent == "")
+      ) {
+        screen.textContent = "Ha Ha Very Funny";
+      }
+    }
+    if (button.id == "decimal") {
+      if (!screen.textContent.includes(".")) screen.textContent += ".";
+    }
+    if (button.id == "pos/neg") {
+      screen.textContent = "-" + screen.textContent;
+    }
+    if (button.id == "percent") {
+      screen.textContent = Number(screen.textContent) / 100;
     }
   })
 );
@@ -45,71 +62,47 @@ const divide = function (x, y) {
   return x / y;
 };
 const equal = function () {
-  bufferText.push(screenText.join(""));
-  updateBuffer();
-  zeroScreen();
-  switch (bufferText[1]) {
+  // figure out operation
+  // run function based on operation
+  // update screen with answer
+  let num1 = Number(buffer.textContent.slice(0, -1));
+  let num2 = Number(screen.textContent);
+  let answer = "";
+
+  switch (buffer.textContent.slice(-1)) {
     case "+":
-      screenText.push(add(bufferText[0], bufferText[2]));
-      console.log(screenText);
-      updateScreen();
+      answer = add(num1, num2);
+      screen.textContent = answer;
+      zeroBuffer();
       break;
     case "-":
-      screenText.push(subtract(bufferText[0], bufferText[2]));
-      console.log(screenText);
-      updateScreen();
+      answer = subtract(num1, num2);
+      screen.textContent = answer;
+      zeroBuffer();
       break;
     case "/":
-      screenText.push(divide(bufferText[0], bufferText[2]));
-      console.log(screenText);
-      updateScreen();
+      if (num2 != 0) {
+        answer = divide(num1, num2);
+        screen.textContent = answer;
+        zeroBuffer();
+      } else {
+        answer = "Nice Try";
+        screen.textContent = answer;
+        zeroBuffer();
+      }
       break;
     case "x":
-      screenText.push(multiply(bufferText[0], bufferText[2]));
-      console.log(screenText);
-      updateScreen();
+      answer = multiply(num1, num2);
+      screen.textContent = answer;
+      zeroBuffer();
       break;
   }
 };
 
-// UPDATE FUNCTIONS
-const updateBuffer = function () {
-  buffer.textContent = bufferText.join(" ");
-};
-const updateScreen = function () {
-  screen.textContent = screenText.join("");
-};
-
 // ZERO FUNCTIONS
 const zeroScreen = function () {
-  screenText = [];
-  screen.textContent = screenText.join();
+  screen.textContent = "";
 };
 const zeroBuffer = function () {
-  bufferText = [];
-  buffer.textContent = bufferText.join();
+  buffer.textContent = "";
 };
-
-// make array using textContent in buffer with textContent on screen
-// array is 3 items [num1, op, num2]
-// check op in array, run function on two numbers depending on the op
-
-// get button id
-// if number add number
-// if operator apply to number
-//
-
-// calculator logic
-// numbers concatenate into "screen"
-// modifers change aspects of numbers
-// operator called, new number for receive
-// if second operator called, replace initial
-// if clear called, remove oeprator and bring back number
-// new number is entered
-// if equal is called, evaluate the solution
-// if new operator is called, evaluate the solution and apply new operator whikle asking for new number
-//
-
-// BONUS
-// Cheat Codes for Gabi
-// divide 1 by 21 alerts "Happy Birthday!"
